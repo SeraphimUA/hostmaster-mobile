ae08e2fde441c1aa63657ead9f07505973441007
SeraphimUA <mikultvua@gmail.com>
2023-03-31 14:37:39 +0300

    Domain Whois Function made

---
+++ whois/whois_script.js
@@ -0,0 +1,29 @@
+function whois_domain_func() {
+  domainElem = document.getElementById("whois_domain");
+  whoisResult = document.getElementById("new_whois_result");
+  domain = "";
+  if (domainElem) {
+    domain = domainElem.value;
+    console.log(domain);
+    url = "https://www.hostmaster.ua/whois/whois.php?_domain_=" + domain;
+    whois_request = new Request(url);
+    fetch(whois_request)
+      .then((response) => {
+        return response.text();
+      })
+      .then((page) => {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(page, "text/html");
+        result = doc.getElementById("whois_result");
+        console.log(result);
+        table_code = document.createElement("pre");
+        table_code.appendChild(result);
+        if (whoisResult.hasChildNodes()) {
+          whoisResult.removeChild(document.querySelector("pre"));
+        }
+        whoisResult.appendChild(table_code);
+        images = table_code.querySelectorAll("img");
+        console.log(images);
+      });
+  }
+}
