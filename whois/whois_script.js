6d1b39c757f1da7a90f41da26b9f431b8f6c1ec3
KimUA2022 <konstantin@mikulenko.net>
2023-07-10 13:29:33 +0300

    Nic handle check page added

--- whois/whois_script.js
+++ whois/whois_script.js
@@ -27,3 +27,33 @@ function whois_domain_func() {
       });
   }
 }
+
+function whois_nichandle_func() {
+  uanicElem = document.getElementById("whois_nichandle");
+  whoisResult = document.getElementById("new_whois_result");
+  domain = "";
+  if (uanicElem) {
+    uanic = domainElem.value;
+    console.log(uanic);
+    url = "https://www.hostmaster.ua/whois/whois.php?_nichandle_=" + uanic;
+    whois_request = new Request(url);
+    fetch(whois_request)
+      .then((response) => {
+        return response.text();
+      })
+      .then((page) => {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(page, "text/html");
+        result = doc.getElementById("whois_result");
+        console.log(result);
+        table_code = document.createElement("pre");
+        table_code.appendChild(result);
+        if (whoisResult.hasChildNodes()) {
+          whoisResult.removeChild(document.querySelector("pre"));
+        }
+        whoisResult.appendChild(table_code);
+        images = table_code.querySelectorAll("img");
+        console.log(images);
+      });
+  }
+}
